c46c37c4eecfe573ec6e6c8f676a81e4f094720f
Eric Mbithi <mbithieri@gmail.com>
2025-08-06 09:19:25 +0300

    Find missing Number

---
+++ missingnumber.py
@@ -0,0 +1,7 @@
+def find_missing_number(nums):
+    n = len(nums)
+    expected_sum = n * (n + 1) // 2
+    actual_sum = sum(nums)
+    return expected_sum - actual_sum
+
+print(find_missing_number([3, 0, 1]))
